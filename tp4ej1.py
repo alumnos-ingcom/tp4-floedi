b5cec40026e24782f9a71023a726652aae331fb1
Florencia Edith Jerez <flor.jerez.06@gmail.com>
2021-05-19 11:45:45 -0300

    Primeras modificaciones

---
+++ tp4ej1.py
@@ -0,0 +1,36 @@
+################
+# Florencia Edith Jerez - @floedi
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+
+
+def ingreso_numeros_enteros():
+    pass
+
+if __name__ == "__main__":
+    prueba()
+    
+
+    
+#     1. Ingreso de números enteros
+# Limitar el ingreso de valores y gestionar los errores que dicha actividad puedan producir
+#es una actividad cotidiana en los primeros programas a desarrollar. Por lo que es un excelente primer ejercicio.
+# 
+# Escribir una funcion que solicite el ingreso de un número entero y vuelva a solicitarlo en caso de ingresar un valor incorrecto
+# 
+# Escribir una funcion que solicite el ingreso de un número entero entre dos valores.
+# 
+# Las tres funciones al rechazar el ingreso de valores incorrectos, deben levantar la excepción
+#IngresoIncorrecto con un mensaje que indique lo sucedido.
+
+
+def ingreso_entero_reintento(mensaje, cantidad_reintentos=5):
+def ingreso_entero_restringido(mensaje,valor_minimo=0, valor_maximo=10):
+
+class IngresoIncorrecto(Exception):
+    """Esta es la Excepcion para el ingreso incorrecto"""
+    pass 
