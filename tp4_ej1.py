389b7d55bdfd0724cd257022e4ee86a58421d142
Florencia Edith Jerez <flor.jerez.06@gmail.com>
2021-05-25 20:11:24 -0300

    Probando excepciones v.1

--- tp4_ej1.py
+++ tp4_ej1.py
@@ -4,17 +4,13 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-    
-def ingreso_entero(mensaje):
+#Funcion que solicite el ingreso de un número entero
+def ingreso_entero(entero):
     """
     Función que muestra un mensaje y agrega un # para
     indicar el ingreso de un número entero.
     """
-mensaje = "Poné un número porfas"
-ingreso = input(mensaje + " #")
-print(f"El número que pusiste es {ingreso}")
-
-
-# print("cuantos años tenés?")
-# edad = input()
-# print(f"wow {edad}, yo tengo cierta edad jijiji")
+try:
+    entero = int(input("Poné un número porfas" + " #"))
+except ValueError as err:
+    print(f"Opa, eso no es un número... Intentá de nuevo")
